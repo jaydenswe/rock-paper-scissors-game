72290e36d444134bed7c28c7639357675dc8b7df
jaydenswe <247421157+jaydenswe@users.noreply.github.com>
2026-03-09 19:28:21 +0000

    Made function input case insensitive

--- index.js
+++ index.js
@@ -1,21 +1,36 @@
-// Create function named getComputerChoice
-function getComputerChoice() {
-  // Use Math.random() to generate random number
-  let x = Math.random();
-  // Use conditional to return one choice
-  let s = "Rock ";
-  if (x > 0.5) {
-    return s;
-  }
-}
-// Use console.log to see if function works so far
-console.log(getComputerChoice());
+// Create variable to track human score
+// let humanScore = 0;
 
-// Create function named getHumanChoice
-function getHumanChoice() {
-  // Prompt user to obtain choice
-  let userInput = prompt("Enter your choice: ");
+// // Create variable to track computer score
+// let computerScore = 0;
+
+// // Create function named getComputerChoice
+// function getComputerChoice() {
+//   // Use Math.random() to generate random number
+//   let x = Math.random();
+//   // Use conditional to return one choice
+//   let s = "Rock ";
+//   if (x > 0.5) {
+//     return s;
+//   }
+// }
+// // Use console.log to see if function works so far
+// console.log(getComputerChoice());
+
+// // Create function named getHumanChoice
+// function getHumanChoice() {
+//   // Prompt user to obtain choice
+//   let userInput = prompt("Enter your choice: ");
+//   return userInput;
+// }
+// // Test to see if pop up appears for prompt
+// console.log(getHumanChoice());
+
+// Create function named playRound
+function playRound(humanChoice) {
+  // Make humanChoice case insensitive
+  let userInput = humanChoice.toLowerCase();
   return userInput;
 }
 
-console.log(getHumanChoice());
+console.log(playRound("ROCK"));
