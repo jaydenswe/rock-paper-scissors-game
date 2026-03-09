e9e6cc74085256d275fe83564712afd5bcdba893
jaydenswe <247421157+jaydenswe@users.noreply.github.com>
2026-03-09 19:11:07 +0000

    Added conditional to return one choice

--- index.js
+++ index.js
@@ -2,7 +2,11 @@
 function getComputerChoice() {
   // Use Math.random() to generate random number
   let x = Math.random();
-  return x;
+  // Use conditional to return one choice
+  let s = "Rock ";
+  if (x > 0.5) {
+    return s;
+  }
 }
 // Use console.log to see if function works so far
 console.log(getComputerChoice());
