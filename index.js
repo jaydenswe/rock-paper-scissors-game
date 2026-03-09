c4f7eeac337fb173b70f75112ea04cb3ffd4ec34
jaydenswe <247421157+jaydenswe@users.noreply.github.com>
2026-03-09 19:07:31 +0000

    Added functionality to generate random number

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 // Add function called getComputerChoice
 function getComputerChoice() {
-  
+  // Use Math.random() to generate random number
+  let x = Math.random();
+  return x;
 }
+// Use console.log to see if function works so far
+console.log(getComputerChoice());
